4f97594f9583d18fe275c949a317fe70a88161ec
Rory Flint <roryflintphoto@gmail.com>
2020-02-14 08:11:43 +0000

    OOP1 complete - tests passing

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -8,13 +8,42 @@
 #   v       v
 # [Car]  [Motorcycle]
 #
-# Each class can simply "pass" for its body. The exercise is about setting up
-# the hierarchy.
-#
-# e.g.
-#
-# class Whatever:
-#     pass
-#
-# Put a comment noting which class is the base class
-#
+
+# Base Class
+class Vehicle():
+    def __init__(self):
+        pass
+
+
+# Ground Vehicle Class
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Car(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+# Flight Vehicle Class
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
